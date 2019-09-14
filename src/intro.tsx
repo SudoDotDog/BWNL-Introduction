eb6a1a38618ee49ddd81d1674c05e70008887b05
WMXPY <wm@mengw.io>
2019-09-14 17:54:01 -0500

    upgrade intro

--- src/intro.tsx
+++ src/intro.tsx
@@ -43,58 +43,8 @@ export class Intro extends React.Component<IntroProps, IntroStates> {
 
     public render() {
 
-        const size: number = this.props.size || 100;
-        const gapSize: number = Math.floor(size / 5);
-
-        const logo: React.ReactElement = React.cloneElement(this.props.logo, {
-            forwarding: this.state.playing,
-            reversing: this.state.ready,
-        } as IntroLogoComponentProps);
-
         return (<React.Fragment>
-            {this.state.covering && <div
-                className={mergeClasses(
-                    this._introStyle.cover,
-                    assertIfTrue(this.state.ready, this._introStyle.ready),
-                )}
-                style={{
-                    zIndex: this.props.zIndex || 30,
-                }}
-            >
-                <div
-                    className={this._introStyle.intro}
-                    style={{
-                        columnGap: `${gapSize}px`,
-                    }}
-                >
-                    <div
-                        className={this._introStyle.icon}
-                        style={{
-                            height: `${size}px`,
-                        }}
-                    >
-                        {logo}
-                    </div>
-                    <div
-                        className={mergeClasses(
-                            this._introStyle.header,
-                            this.props.headerClassName,
-                        )}
-                        style={this._getHeaderStyle()}
-                    >
-                        {this.props.header}
-                    </div>
-                    <div
-                        className={mergeClasses(
-                            this._introStyle.body,
-                            this.props.bodyClassName,
-                        )}
-                        style={this._getBodyStyle()}
-                    >
-                        {this.props.body}
-                    </div>
-                </div>
-            </div>}
+            {this._renderCover()}
             <div
                 className={this.props.className}
                 style={{
@@ -105,6 +55,65 @@ export class Intro extends React.Component<IntroProps, IntroStates> {
                 {this.props.children}
             </div>
         </React.Fragment>);
+    }
+
+    private _renderCover() {
+
+        if (!this.state.covering) {
+            return null;
+        }
+
+        const size: number = this.props.size || 100;
+        const gapSize: number = Math.floor(size / 5);
+
+        const logo: React.ReactElement = React.cloneElement(this.props.logo, {
+            forwarding: this.state.playing,
+            reversing: this.state.ready,
+        } as IntroLogoComponentProps);
+
+        return (<div
+            className={mergeClasses(
+                this._introStyle.cover,
+                assertIfTrue(this.state.ready, this._introStyle.ready),
+            )}
+            style={{
+                zIndex: this.props.zIndex || 30,
+            }}
+        >
+            <div
+                className={this._introStyle.intro}
+                style={{
+                    columnGap: `${gapSize}px`,
+                }}
+            >
+                <div
+                    className={this._introStyle.icon}
+                    style={{
+                        height: `${size}px`,
+                    }}
+                >
+                    {logo}
+                </div>
+                <div
+                    className={mergeClasses(
+                        this._introStyle.header,
+                        this.props.headerClassName,
+                    )}
+                    style={this._getHeaderStyle()}
+                >
+                    {this.props.header}
+                </div>
+                <div
+                    className={mergeClasses(
+                        this._introStyle.body,
+                        this.props.bodyClassName,
+                    )}
+                    style={this._getBodyStyle()}
+                >
+                    {this.props.body}
+                </div>
+            </div>
+        </div>);
     }
 
     private _getHeaderStyle(): React.CSSProperties {
