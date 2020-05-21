d53e6ee387200852e5685e5b1de4b172e1cba5e6
WMXPY <wm@mengw.io>
2020-05-21 12:17:45 -0500

    add const

---
+++ src/constraint.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace Intro
+ * @description Constraint
+ */
+
+export const DEFAULT_DELAY: number = 100;
+export const DEFAULT_DURATION: number = 2000;
+export const DEFAULT_PHASE: number = 300;
